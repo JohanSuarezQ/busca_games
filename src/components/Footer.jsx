c2ace74916b6edda24d1482de4b45ed2bbf2eeff
Johán Suárez <32972612+JohanSuarezQ@users.noreply.github.com>
2020-09-11 11:56:55 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import React from "react";
 
-import GithubLogo from '../assets/github.svg'
-import LinkedinLogo from '../assets/linkedin.svg'
-import InstagramLogo from '../assets/instagram.svg'
-
+import GithubLogo from "../assets/github.svg";
+import LinkedinLogo from "../assets/linkedin.svg";
+import InstagramLogo from "../assets/instagram.svg";
 
 export const Footer = () => {
-    return (
-        <footer className = 'footer__container'>
-            <p>De un gamer para otro. Hecho por JohánSQ</p>
-            <ul className = 'footer__rrss'>
-                <li><a target="_blank" href = 'https://github.com/JohanSuarezQ'><img src={GithubLogo} alt=""/></a></li>
-                <li><a target="_blank" href = 'https://www.linkedin.com/in/johansq/'><img src={LinkedinLogo} alt=""/></a></li>
-                <li><a target="_blank" href = 'https://www.instagram.com/johansuarezq/'><img src={InstagramLogo} alt=""/></a></li>
-            </ul>
-        </footer>
-    )
-}
+  return (
+    <footer className="footer__container">
+      <p>De un gamer para otro. Hecho por JohánSQ</p>
+      <ul className="footer__rrss">
+        <li>
+          <a target="_blank" href="https://github.com/JohanSuarezQ">
+            <img src={GithubLogo} alt="" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://www.linkedin.com/in/johansq/">
+            <img src={LinkedinLogo} alt="" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://www.instagram.com/johansuarezq/">
+            <img src={InstagramLogo} alt="" />
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
